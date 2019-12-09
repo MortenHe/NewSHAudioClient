de6785f6aad084f8dbb202b0707acbdb1ec9b906
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2019-12-09 12:13:15 +0100

    luis config

---
+++ src/environments/environment.luis.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  serverUrl: 'http://192.168.0.233',
+  wssUrl: 'ws://192.168.0.233:9090',
+  modes: [{
+    id: 'sh',
+    label: 'SH',
+    active: true
+  },
+  {
+    id: 'mh',
+    label: 'MH',
+    active: true
+  },
+  {
+    id: 'kids',
+    label: 'Kids',
+    active: true
+  }]
+};

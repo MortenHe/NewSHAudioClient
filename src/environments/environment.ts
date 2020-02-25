56dca78f5badfa773a9de816ccf80b1400a473b2
Martin Helfer <martin-helfer@gmx.de>
2020-02-25 20:58:15 +0100

    config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   envName: 'Steffis Musik Player',
   production: false,
-  //serverUrl: 'http://localhost:9090',
-  serverUrl: 'http://132.230.25.79',
-  //wssUrl: 'ws://localhost:9090',
-  wssUrl: 'ws://132.230.25.79:9090',
+  //serverUrl: 'http://132.230.25.79',
+  serverUrl: 'http://localhost:9090',
+  //wssUrl: 'ws://132.230.25.79:9090',
+  wssUrl: 'ws://localhost:9090',
   modes: [{
     id: 'sh',
     label: 'SH',

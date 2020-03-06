5d48d0631c779155e93037f3f109855e1ec0fb38
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2020-03-06 16:43:44 +0100

    environment dist

---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  envName: 'Steffis Musik Player',
+  production: false,
+  serverUrl: 'http://132.230.25.79',
+  wssUrl: 'ws://132.230.25.79:9090',
+  modes: [{
+    id: 'sh',
+    label: 'SH',
+    active: true
+  },
+  {
+    id: 'mh',
+    label: 'MH',
+    active: true
+  },
+  {
+    id: 'kids',
+    label: 'Kids',
+    active: true
+  }]
+};

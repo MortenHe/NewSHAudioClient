1d31f18315f322936ec41a58bc60cfec22195421
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2020-07-25 22:48:57 +0200

    sh config

---
+++ src/environments/environment.sh.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  envName: 'SH Musik Player',
+  production: true,
+  serverUrl: 'http://192.168.0.224/php',
+  wssUrl: 'ws://192.168.0.224:9090',
+  modes: [{
+    id: 'sh',
+    label: 'SH',
+    active: true
+  },
+  {
+    id: 'mh',
+    label: 'MH',
+    active: true
+  },
+  {
+    id: 'kids',
+    label: 'Kids',
+    active: true
+  }]
+};

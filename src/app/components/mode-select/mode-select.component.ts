6484d93e3f9f25be3927d2710bce2a97ae44cf18
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2021-05-16 14:02:35 +0200

    Mode-Auswahl per Select

--- src/app/components/mode-select/mode-select.component.ts
+++ src/app/components/mode-select/mode-select.component.ts
@@ -1,42 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { BackendService } from 'src/app/services/backend.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'mode-select',
-  templateUrl: './mode-select.component.html',
-  styleUrls: ['./mode-select.component.scss']
+    selector: 'mode-select',
+    templateUrl: './mode-select.component.html',
+    styleUrls: ['./mode-select.component.scss']
 })
 export class ModeSelectComponent implements OnInit {
 
-  //Liste des AudioModes
-  audioModes: any[] = [];
+    //Liste des AudioModes
+    audioModes: any[] = [];
 
-  //Aktueller Audiomodus (sh, mh, kids)
-  audioMode = "";
+    //Selectbox fuer Audiomodus (sh, mh, kids)
+    audioModeSelect: FormControl = new FormControl();
 
-  constructor(private bs: BackendService) { }
+    constructor(private bs: BackendService) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    //Liste der audioModes Abo
-    this.bs.getAudioModes().subscribe((audioModes: any[]) => {
-      this.audioModes = audioModes;
-    });
+        //Liste der audioModes Abo
+        this.bs.getAudioModes().subscribe((audioModes: any[]) => {
+            this.audioModes = audioModes;
+        });
 
-    //aktuellen audioMode abonnieren
-    this.bs.getAudioMode().subscribe(audioMode => {
-      this.audioMode = audioMode;
-    });
-  }
+        //aktuellen audioMode abonnieren und passenden Select-Wert auswaehlen
+        this.bs.getAudioMode().subscribe(audioMode => {
+            const userModeIndex = this.audioModes.findIndex((obj) => {
+                return obj.id === audioMode;
+            });
+            this.audioModeSelect.setValue(this.audioModes[userModeIndex], { emitEvent: false });
+        });
 
-  //Audiomodus setzen (sh, mh, kids)
-  setAudioMode(audioMode) {
-    this.bs.sendMessage({
-      type: "set-audio-mode",
-      value: {
-        "audioMode": audioMode
-      }
-    });
-  }
+        //Aenderung des audioModes an Server melden
+        this.audioModeSelect.valueChanges.subscribe(audioMode => {
+            this.bs.sendMessage({
+                type: "set-audio-mode",
+                value: {
+                    "audioMode": audioMode.id
+                }
+            });
+        });
+    }
 }
